de13c1c41f69643279ae87a592df757eec5917dc
jayrick-gacayan-test <jayrick-gacayan@kodakollectiv.com>
2022-05-18 15:05:18 +0800

    Correcting errors for deployment

--- config/index.js
+++ config/index.js
@@ -15,7 +15,7 @@ module.exports = {
   production: {
     HOST: "ec2-44-195-169-163.compute-1.amazonaws.com",
     USER: "iqnxeoldobxprt",
-    PASSWORD: "123",
+    PASSWORD: "af08dd98c0c4c9910ccb9e51ae3d6280b1c244ec2eeb691b1df3fef1834f6f06",
     DATABASE: "d83a54g70ldnl1",
     dialect: "postgres",
     pool: {
